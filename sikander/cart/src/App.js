c1a9a7e79d17a637095c1d287a843ff482ba45fe
nadeem <na.abbas001@gmail.com>
2020-09-20 14:27:44 +0500

    changes

---
+++ sikander/cart/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">slik chopping cart</a>
+      </header>
+      <main>
+        product list
+      </main>
+      <footer>
+        All right
+      </footer>
+      
+      </div>
+  );
+}
+
+export default App;
